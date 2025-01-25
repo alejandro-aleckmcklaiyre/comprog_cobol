d8d287df50588d3daa65fecc2a2b2c3caca6ebc1
alejandro-aleck <aleck.alejandro04@gmail.com>
2025-01-26 01:46:30 +0800

    add new file

--- file_handling.cbl
+++ file_handling.cbl
@@ -25,15 +25,27 @@
                DISPLAY "       FILE HANDLING MAIN MENU         "
                DISPLAY "======================================="
                DISPLAY "1. Add New Album Record (Aleck)"
-               DISPLAY "2. Exit"
+               DISPLAY "2. Display the list (Jasmine)"
+               DISPLAY "3. Update existing Recid (Loise)"
+               DISPLAY "4. Delete a record (Ash)"
+               DISPLAY "5. Search for a record (Mik)"
+               Display "6. Exit"
                DISPLAY "======================================="
-               DISPLAY "Enter your choice (1-2): "
+               DISPLAY "Enter your choice (1-6): "
                ACCEPT WS-USER-CHOICE
 
                EVALUATE WS-USER-CHOICE
                    WHEN 1
                        PERFORM ADD-NEW-RECORD
                    WHEN 2
+                       DISPLAY "Displaying the records"
+                   WHEN 3
+                       DISPLAY "Update the records"
+                   WHEN 4
+                       DISPLAY "Delete the records"
+                   WHEN 5
+                       DISPLAY "View the records"
+                   WHEN 6
                        DISPLAY "Exiting Program. Goodbye!"
                    WHEN OTHER
                        DISPLAY "Invalid choice. Please try again."
