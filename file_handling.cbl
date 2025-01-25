471a85d759ace6894deb376c4ef565d978250b57
alejandro-aleck <aleck.alejandro04@gmail.com>
2025-01-26 01:23:49 +0800

    add a todo.

---
+++ file_handling.cbl
@@ -0,0 +1,19 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FileHandlingProgram.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 WS-RECORDS.
+       05 WS-RECORD PIC X(100).
+
+       PROCEDURE DIVISION.
+           DISPLAY "File Handling Program in COBOL".
+
+      * TODO: Assigned Tasks
+      * Aleck: Add new record
+      * Jas: Display all records
+      * Loise: Update an existing record
+      * Ash: Delete a record
+      * Mik: Search for a specific record
+
+           STOP RUN.
