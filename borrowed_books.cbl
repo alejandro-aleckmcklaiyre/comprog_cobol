91ea4bb23e41240372901bcc51e6c3fb1ae4fb7d
deleon-jasminerobelle <jd8312288@gmail.com>
2025-01-27 01:27:24 +0800

    update file

--- borrowed_books.cbl
+++ borrowed_books.cbl
@@ -24,6 +24,8 @@
        01  WS-Continue          PIC X VALUE 'Y'.
        01  EOF                  PIC X VALUE 'N'.
        01  FileStatus           PIC X(2).
+       01  WS-DeleteTitle       PIC X(50).
+       01  WS-BookFound         PIC X VALUE 'N'.
 
        PROCEDURE DIVISION.
 
@@ -41,7 +43,8 @@
            PERFORM UNTIL WS-Continue = 'N'
                DISPLAY "1. Add a new borrowed book"
                DISPLAY "2. Display borrowed books"
-               DISPLAY "3. Exit"
+               DISPLAY "3. Delete Book"
+               DISPLAY "4. Exit"
                ACCEPT WS-Choice
                EVALUATE WS-Choice
                    WHEN 1
@@ -49,6 +52,8 @@
                    WHEN 2
                        PERFORM DISPLAY-BOOKS
                    WHEN 3
+                       PERFORM DELETE-BOOK
+                   WHEN 4
                        MOVE 'N' TO WS-Continue
                    WHEN OTHER
                        DISPLAY "Invalid choice, please try again."
@@ -73,7 +78,7 @@
                NOT INVALID KEY
                    DISPLAY "Book successfully added."
            END-WRITE.
-
+       
        DISPLAY-BOOKS.
            DISPLAY "List of borrowed books:"
            MOVE 'N' TO EOF  *> Reset EOF flag before reading
@@ -90,5 +95,34 @@
                END-READ
            END-PERFORM
            CLOSE BorrowedBooksFile.
+       
+       DELETE-BOOK.
+           DISPLAY "Enter the title of the book to delete: "
+           ACCEPT WS-DeleteTitle
+           MOVE 'N' TO WS-BookFound
+           MOVE 'N' TO EOF  *> Reset EOF flag before reading
+           PERFORM UNTIL EOF = 'Y'
+               READ BorrowedBooksFile INTO BorrowedBookRecord
+                   AT END
+                       MOVE 'Y' TO EOF
+                   NOT AT END
+                       IF WS-DeleteTitle = BookTitle
+                           DELETE BorrowedBooksFile
+                               INVALID KEY
+                                   DISPLAY "Error: Book not found."
+                               NOT INVALID KEY
+                       DISPLAY "Book successfully deleted."
+                       PERFORM DISPLAY-BOOKS
+
+                                   MOVE 'Y' TO WS-BookFound
+                           END-DELETE
+                       END-IF
+               END-READ
+           END-PERFORM
+           IF WS-BookFound = 'N' 
+               DISPLAY "Error: Book not found."
+
+               DISPLAY "Error: Book not found."
+           END-IF.
 
        END PROGRAM BorrowedBooks.
